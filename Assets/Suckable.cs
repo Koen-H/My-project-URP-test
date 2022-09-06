84138051184bad132508d0695af6796bb778821c
Koen H <49477924+Koen-H@users.noreply.github.com>
2022-09-06 16:06:47 +0200

    The sucking machine keeps track of sucked items and makes it smaller

--- Assets/Suckable.cs
+++ Assets/Suckable.cs
@@ -5,23 +5,31 @@ using UnityEngine;
 public class Suckable : MonoBehaviour
 {
     Rigidbody rigidbody;
+    public bool isShrinking;
+    public float shrinkSpeed;//Needs to be below 1
 
     private void Start()
     {
         rigidbody = GetComponent<Rigidbody>();
     }
+    private void Update()
+    {
+        if (isShrinking) Shrink();
+    }
+
 
     public void Suck(Vector3 _origin, float _suckPower)
     {
-
+        if(rigidbody != null) { 
         //Vector3 velocity = Vector3.MoveTowards(this.transform.position, _origin, 1) * _suckPower;
         Vector3 velocity = (_origin - this.transform.position).normalized * _suckPower;
         rigidbody.AddForce(velocity);
-        Debug.Log(velocity);
+        }
     }
-    public void Blow()
+    
+    public void Shrink()
     {
-
+        this.transform.localScale = new Vector3(transform.localScale.x, transform.localScale.y, transform.localScale.z) * shrinkSpeed;
     }
 
 }

361c3d6b64a23589a5bc23b1ae4ab800968b60dd
Koen H <49477924+Koen-H@users.noreply.github.com>
2022-09-09 14:42:02 +0200

    There is a first build with actual tweaked performence
    Make sure to enable 2024k override when building!

---
+++ Build-APK/OceanCleaner_BackUpThisFolder_ButDontShipItWithYourGame/il2cppOutput/Assembly-CSharp_CodeGen.c
@@ -0,0 +1,333 @@
+﻿#include "pch-c.h"
+#ifndef _MSC_VER
+# include <alloca.h>
+#else
+# include <malloc.h>
+#endif
+
+
+#include "codegen/il2cpp-codegen-metadata.h"
+
+
+
+
+
+// 0x00000001 GameManager GameManager::get_Instance()
+extern void GameManager_get_Instance_m076FE4D98E785B5AEE0B4C360C7857F824E7FBD0 (void);
+// 0x00000002 System.Void GameManager::Awake()
+extern void GameManager_Awake_m4B6E8E2AF58C95C9A2A0C4637A34AE0892CB637F (void);
+// 0x00000003 System.Void GameManager::Start()
+extern void GameManager_Start_m87A71D65F3171A58DBDDBFB03832ADA65643D0E2 (void);
+// 0x00000004 System.Collections.IEnumerator GameManager::StartTimer()
+extern void GameManager_StartTimer_m7B516AC4FF69FD0A4DD8904687D54415474656AC (void);
+// 0x00000005 System.Void GameManager::.ctor()
+extern void GameManager__ctor_mF453CED520617BFB65C52405A964E06CF17DB368 (void);
+// 0x00000006 System.Void GameManager/<StartTimer>d__8::.ctor(System.Int32)
+extern void U3CStartTimerU3Ed__8__ctor_m3258309E589569BB2FC22BB9CDE9C39958B10283 (void);
+// 0x00000007 System.Void GameManager/<StartTimer>d__8::System.IDisposable.Dispose()
+extern void U3CStartTimerU3Ed__8_System_IDisposable_Dispose_m35B3F7484E182273B6295FAC0B164ED08E1600A8 (void);
+// 0x00000008 System.Boolean GameManager/<StartTimer>d__8::MoveNext()
+extern void U3CStartTimerU3Ed__8_MoveNext_m2AC827F4854F8685A9B8CF2D20995C999C8E41D6 (void);
+// 0x00000009 System.Object GameManager/<StartTimer>d__8::System.Collections.Generic.IEnumerator<System.Object>.get_Current()
+extern void U3CStartTimerU3Ed__8_System_Collections_Generic_IEnumeratorU3CSystem_ObjectU3E_get_Current_m39CDD1E9D9046AEC8A7572FEB12B048D5278BE22 (void);
+// 0x0000000A System.Void GameManager/<StartTimer>d__8::System.Collections.IEnumerator.Reset()
+extern void U3CStartTimerU3Ed__8_System_Collections_IEnumerator_Reset_m8EB41D9FDA89A1D94E780BA5A0DBF95163A7EECE (void);
+// 0x0000000B System.Object GameManager/<StartTimer>d__8::System.Collections.IEnumerator.get_Current()
+extern void U3CStartTimerU3Ed__8_System_Collections_IEnumerator_get_Current_m7F939BABD8C3EDC74E6914BBD278C512E199915B (void);
+// 0x0000000C System.Void HookController::Start()
+extern void HookController_Start_mEEF3B5805E25FC4DFD65631964A4A977C346DCFB (void);
+// 0x0000000D System.Void HookController::OnTriggerEnter(UnityEngine.Collider)
+extern void HookController_OnTriggerEnter_m3A1EDBDDA0D75F675CA2F9598CCEE2B6E3B3EA25 (void);
+// 0x0000000E System.Void HookController::Retrieve()
+extern void HookController_Retrieve_mEAB382DAAC16AE0E159ED71E11CFAF4ADEF187F4 (void);
+// 0x0000000F System.Void HookController::Move()
+extern void HookController_Move_m571724CC65421DF108A7C201936BF69499768C93 (void);
+// 0x00000010 System.Void HookController::FixedUpdate()
+extern void HookController_FixedUpdate_m699F292D5AAE7403D922D66CA2F24554F759EB5C (void);
+// 0x00000011 System.Boolean HookController::IsAttached()
+extern void HookController_IsAttached_m760A9C1886636EBC0A5D826FBBBBC58A8D2DEAB5 (void);
+// 0x00000012 System.Void HookController::.ctor()
+extern void HookController__ctor_m5929B2FCD386423ECBC773E2D6838A11688EFAA9 (void);
+// 0x00000013 System.Void HookGunController::Start()
+extern void HookGunController_Start_m00017A23E73654A49408777A1FAB6C1277485FA5 (void);
+// 0x00000014 System.Void HookGunController::Update()
+extern void HookGunController_Update_mB464EBFC8E79BBBAFE2CFE8136FA2F0C4DB7F9D4 (void);
+// 0x00000015 System.Void HookGunController::DrawGizmos()
+extern void HookGunController_DrawGizmos_m04594A5FF737776074A89895C2799B3132AAAB61 (void);
+// 0x00000016 UnityEngine.Vector3 HookGunController::CalculateDirection()
+extern void HookGunController_CalculateDirection_m93249784EA8C76FFA1D1E877C34D765BC6D12946 (void);
+// 0x00000017 System.Single HookGunController::PullFactor()
+extern void HookGunController_PullFactor_mB12D5261A815F1C3FE9B7CA6D3D4883080EEFD90 (void);
+// 0x00000018 System.Void HookGunController::.ctor()
+extern void HookGunController__ctor_m486E7E399001A70862F5B11260B3BB5933A6A9BD (void);
+// 0x00000019 System.Void PlayerController::Start()
+extern void PlayerController_Start_m1D83076E8B136A71051F2F02545EE04947D3A8CF (void);
+// 0x0000001A System.Void PlayerController::Update()
+extern void PlayerController_Update_m1F4051EB5BCBCCE5EEE2E3E49B7E278C3B14EC33 (void);
+// 0x0000001B System.Void PlayerController::FixedUpdate()
+extern void PlayerController_FixedUpdate_m6D906D8B13844542B81CC49BA19760F747CEC8C0 (void);
+// 0x0000001C System.Void PlayerController::.ctor()
+extern void PlayerController__ctor_mDDAB7C7D82E1A5B3E6C197B1AB9D653DFE554F33 (void);
+// 0x0000001D UnityEngine.RaycastHit[] ConeCast::ConeCastAll(UnityEngine.Physics,UnityEngine.Vector3,System.Single,UnityEngine.Vector3,System.Single,System.Single)
+extern void ConeCast_ConeCastAll_mB8B6EF562C96E08ACB9F20FCE152511A1D7FCB7B (void);
+// 0x0000001E System.Void GrapplingHookShoot::Start()
+extern void GrapplingHookShoot_Start_m75BDA03709395D21FE3BE1672B6CEBD6604710E1 (void);
+// 0x0000001F System.Void GrapplingHookShoot::Shoot()
+extern void GrapplingHookShoot_Shoot_mBA6C93C95FC156E09E578389A7F76A97F9EC909D (void);
+// 0x00000020 System.Void GrapplingHookShoot::Update()
+extern void GrapplingHookShoot_Update_mD1101C5E82394E857E30C0C620190B5623CD6260 (void);
+// 0x00000021 System.Void GrapplingHookShoot::CheckForRetrieve()
+extern void GrapplingHookShoot_CheckForRetrieve_mCA5DC4FDCB647E0E2E5E6EAF8D15B3DD39C27D53 (void);
+// 0x00000022 System.Void GrapplingHookShoot::LateUpdate()
+extern void GrapplingHookShoot_LateUpdate_m6F40F507C743D717B355BFCB5FA2F6D5F1FEF35B (void);
+// 0x00000023 System.Void GrapplingHookShoot::DrawRope()
+extern void GrapplingHookShoot_DrawRope_m04B017F53EFBA01DDBD324E8F7FDE195CEE7DA18 (void);
+// 0x00000024 System.Void GrapplingHookShoot::Pull(System.Single)
+extern void GrapplingHookShoot_Pull_m9C8A5F370C4CF4670D8426865C0948A7FB29AF21 (void);
+// 0x00000025 System.Void GrapplingHookShoot::LetGo()
+extern void GrapplingHookShoot_LetGo_m7442B9972FB41284BE241C7592D93ADC055386FD (void);
+// 0x00000026 System.Void GrapplingHookShoot::ReturnInGun()
+extern void GrapplingHookShoot_ReturnInGun_mD83E21C2B6498983C8CFE630447F81DA86A6E411 (void);
+// 0x00000027 System.Void GrapplingHookShoot::.ctor()
+extern void GrapplingHookShoot__ctor_mE28A78EC311D207EF2135B7725DB2AC6FA5323A8 (void);
+// 0x00000028 Haptic Haptic::get_Instance()
+extern void Haptic_get_Instance_m08B701575323EC53472DCEB31E639F2B74594403 (void);
+// 0x00000029 System.Void Haptic::Awake()
+extern void Haptic_Awake_m13F703C316116106EF077FF8D7492BB7BB656814 (void);
+// 0x0000002A System.Void Haptic::SendHapticsLeftController(System.Single,System.Single)
+extern void Haptic_SendHapticsLeftController_m3FB636C0F64B220C414510708DF93656E4EB7AA5 (void);
+// 0x0000002B System.Void Haptic::SendHapticsRightController(System.Single,System.Single)
+extern void Haptic_SendHapticsRightController_m765E5DD565AE8A77B1BEB0988939628C3E9A1492 (void);
+// 0x0000002C System.Void Haptic::.ctor()
+extern void Haptic__ctor_m2F0C7BDCD2320E80E9F7ACED8E51A5E84CEC216E (void);
+// 0x0000002D System.Void Suckable::Start()
+extern void Suckable_Start_m835ABDEF2DB4D13CD82DFC635317369CCE59EA68 (void);
+// 0x0000002E System.Void Suckable::Update()
+extern void Suckable_Update_m646795073D2F6EBF69BC6814ED5D1CD70184E983 (void);
+// 0x0000002F System.Void Suckable::FixedUpdate()
+extern void Suckable_FixedUpdate_m66B57094834866670DBEC7B358D93C6A6586CB12 (void);
+// 0x00000030 System.Void Suckable::DeleteItem()
+extern void Suckable_DeleteItem_m54C9F5B23D21A3C938DF95DB65E72ACF289F8F9A (void);
+// 0x00000031 System.Void Suckable::Suck(UnityEngine.Vector3,System.Single,SuckingMachineController)
+extern void Suckable_Suck_mD553A7856DEE7C905925426F11DA0AEDBF7C9001 (void);
+// 0x00000032 System.Void Suckable::Shrink()
+extern void Suckable_Shrink_mAF621E3A34371F4947D902F6980199F34A01EDAD (void);
+// 0x00000033 System.Void Suckable::Flow()
+extern void Suckable_Flow_mE874E3E39B2F4DCA289FC02A01D35740FA7E6FC6 (void);
+// 0x00000034 System.Void Suckable::Swooshes(System.Single,System.Single)
+extern void Suckable_Swooshes_m754AF3D52BBB73DEECEA16D8CF3E973B4C4D4424 (void);
+// 0x00000035 System.Void Suckable::.ctor()
+extern void Suckable__ctor_mF233F4A12E9F47A487E4540F400B4DB4A93F71DE (void);
+// 0x00000036 System.Void SuckingMachineCollectionController::Start()
+extern void SuckingMachineCollectionController_Start_mB8268FF23825787BBCBF66687E5C473E2ACB57B1 (void);
+// 0x00000037 System.Void SuckingMachineCollectionController::OnTriggerEnter(UnityEngine.Collider)
+extern void SuckingMachineCollectionController_OnTriggerEnter_mC9F3365F2F2BB96648495B9E875B6595DB3E2478 (void);
+// 0x00000038 System.Void SuckingMachineCollectionController::.ctor()
+extern void SuckingMachineCollectionController__ctor_mFDDEE9CA9FE5D647F692CB2532CC9C3C26C39997 (void);
+// 0x00000039 System.Void SuckingMachineController::Start()
+extern void SuckingMachineController_Start_mA645913FDB01B274F50AAB88D3BC9ADBB2CB8A3A (void);
+// 0x0000003A System.Void SuckingMachineController::CalculateBarCapacity()
+extern void SuckingMachineController_CalculateBarCapacity_m2BF3F48E4B904547FF5986504B474433378AA68B (void);
+// 0x0000003B System.Void SuckingMachineController::Update()
+extern void SuckingMachineController_Update_mE389D74A1159D5405AEC716DF57038C5BABB2DF5 (void);
+// 0x0000003C System.Void SuckingMachineController::UpdateCapacityBar()
+extern void SuckingMachineController_UpdateCapacityBar_mBEDB3979B10AB0D42A763A7E28C16AE2909BD98D (void);
+// 0x0000003D System.Void SuckingMachineController::FixedUpdate()
+extern void SuckingMachineController_FixedUpdate_m7E59043D06577CE273A809CE71F390A3FE747AC1 (void);
+// 0x0000003E System.Void SuckingMachineController::Sucking()
+extern void SuckingMachineController_Sucking_m7C858319D15D2DB8213F153780110C138C395D8C (void);
+// 0x0000003F System.Void SuckingMachineController::CooldownMechanics()
+extern void SuckingMachineController_CooldownMechanics_mCB4DF86803FF5A264CFF8EFE4FA5F406BAD3F2FE (void);
+// 0x00000040 System.Void SuckingMachineController::Shoot()
+extern void SuckingMachineController_Shoot_m2B89AE7E9056D567BDFC6B829E8311907C963761 (void);
+// 0x00000041 System.Void SuckingMachineController::.ctor()
+extern void SuckingMachineController__ctor_m3F6175B2B0561D16A00A73809E074A4B87E62B05 (void);
+// 0x00000042 System.Void TrashGenerator::Start()
+extern void TrashGenerator_Start_mFA7B31CCD86CF6C68010EE464FCBF5B2DDA0B004 (void);
+// 0x00000043 System.Void TrashGenerator::SpawnItem()
+extern void TrashGenerator_SpawnItem_m01F329A16D1A376794888B96BA5ACF21BB140ECA (void);
+// 0x00000044 UnityEngine.Vector3 TrashGenerator::CalculateSpread(System.Single)
+extern void TrashGenerator_CalculateSpread_m0C4DB34964AEDAFE7A8D6CE6A7D4584AECE3B12F (void);
+// 0x00000045 System.Void TrashGenerator::FixedUpdate()
+extern void TrashGenerator_FixedUpdate_m04EEDEB186EDB4D6D377B896649159886FDD3F0F (void);
+// 0x00000046 System.Void TrashGenerator::Update()
+extern void TrashGenerator_Update_m32C29032189BFDD4654EA71A4A6F1A68FF300375 (void);
+// 0x00000047 System.Void TrashGenerator::.ctor()
+extern void TrashGenerator__ctor_m58BF269AEF0865D72D36AA6A76C20108273F9A98 (void);
+// 0x00000048 System.Void Readme::.ctor()
+extern void Readme__ctor_m69C325C4C171DCB0312B646A9034AA91EA8C39C6 (void);
+// 0x00000049 System.Void Readme/Section::.ctor()
+extern void Section__ctor_m5F732533E4DFC0167D965E5F5DB332E46055399B (void);
+static Il2CppMethodPointer s_methodPointers[73] = 
+{
+	GameManager_get_Instance_m076FE4D98E785B5AEE0B4C360C7857F824E7FBD0,
+	GameManager_Awake_m4B6E8E2AF58C95C9A2A0C4637A34AE0892CB637F,
+	GameManager_Start_m87A71D65F3171A58DBDDBFB03832ADA65643D0E2,
+	GameManager_StartTimer_m7B516AC4FF69FD0A4DD8904687D54415474656AC,
+	GameManager__ctor_mF453CED520617BFB65C52405A964E06CF17DB368,
+	U3CStartTimerU3Ed__8__ctor_m3258309E589569BB2FC22BB9CDE9C39958B10283,
+	U3CStartTimerU3Ed__8_System_IDisposable_Dispose_m35B3F7484E182273B6295FAC0B164ED08E1600A8,
+	U3CStartTimerU3Ed__8_MoveNext_m2AC827F4854F8685A9B8CF2D20995C999C8E41D6,
+	U3CStartTimerU3Ed__8_System_Collections_Generic_IEnumeratorU3CSystem_ObjectU3E_get_Current_m39CDD1E9D9046AEC8A7572FEB12B048D5278BE22,
+	U3CStartTimerU3Ed__8_System_Collections_IEnumerator_Reset_m8EB41D9FDA89A1D94E780BA5A0DBF95163A7EECE,
+	U3CStartTimerU3Ed__8_System_Collections_IEnumerator_get_Current_m7F939BABD8C3EDC74E6914BBD278C512E199915B,
+	HookController_Start_mEEF3B5805E25FC4DFD65631964A4A977C346DCFB,
+	HookController_OnTriggerEnter_m3A1EDBDDA0D75F675CA2F9598CCEE2B6E3B3EA25,
+	HookController_Retrieve_mEAB382DAAC16AE0E159ED71E11CFAF4ADEF187F4,
+	HookController_Move_m571724CC65421DF108A7C201936BF69499768C93,
+	HookController_FixedUpdate_m699F292D5AAE7403D922D66CA2F24554F759EB5C,
+	HookController_IsAttached_m760A9C1886636EBC0A5D826FBBBBC58A8D2DEAB5,
+	HookController__ctor_m5929B2FCD386423ECBC773E2D6838A11688EFAA9,
+	HookGunController_Start_m00017A23E73654A49408777A1FAB6C1277485FA5,
+	HookGunController_Update_mB464EBFC8E79BBBAFE2CFE8136FA2F0C4DB7F9D4,
+	HookGunController_DrawGizmos_m04594A5FF737776074A89895C2799B3132AAAB61,
+	HookGunController_CalculateDirection_m93249784EA8C76FFA1D1E877C34D765BC6D12946,
+	HookGunController_PullFactor_mB12D5261A815F1C3FE9B7CA6D3D4883080EEFD90,
+	HookGunController__ctor_m486E7E399001A70862F5B11260B3BB5933A6A9BD,
+	PlayerController_Start_m1D83076E8B136A71051F2F02545EE04947D3A8CF,
+	PlayerController_Update_m1F4051EB5BCBCCE5EEE2E3E49B7E278C3B14EC33,
+	PlayerController_FixedUpdate_m6D906D8B13844542B81CC49BA19760F747CEC8C0,
+	PlayerController__ctor_mDDAB7C7D82E1A5B3E6C197B1AB9D653DFE554F33,
+	ConeCast_ConeCastAll_mB8B6EF562C96E08ACB9F20FCE152511A1D7FCB7B,
+	GrapplingHookShoot_Start_m75BDA03709395D21FE3BE1672B6CEBD6604710E1,
+	GrapplingHookShoot_Shoot_mBA6C93C95FC156E09E578389A7F76A97F9EC909D,
+	GrapplingHookShoot_Update_mD1101C5E82394E857E30C0C620190B5623CD6260,
+	GrapplingHookShoot_CheckForRetrieve_mCA5DC4FDCB647E0E2E5E6EAF8D15B3DD39C27D53,
+	GrapplingHookShoot_LateUpdate_m6F40F507C743D717B355BFCB5FA2F6D5F1FEF35B,
+	GrapplingHookShoot_DrawRope_m04B017F53EFBA01DDBD324E8F7FDE195CEE7DA18,
+	GrapplingHookShoot_Pull_m9C8A5F370C4CF4670D8426865C0948A7FB29AF21,
+	GrapplingHookShoot_LetGo_m7442B9972FB41284BE241C7592D93ADC055386FD,
+	GrapplingHookShoot_ReturnInGun_mD83E21C2B6498983C8CFE630447F81DA86A6E411,
+	GrapplingHookShoot__ctor_mE28A78EC311D207EF2135B7725DB2AC6FA5323A8,
+	Haptic_get_Instance_m08B701575323EC53472DCEB31E639F2B74594403,
+	Haptic_Awake_m13F703C316116106EF077FF8D7492BB7BB656814,
+	Haptic_SendHapticsLeftController_m3FB636C0F64B220C414510708DF93656E4EB7AA5,
+	Haptic_SendHapticsRightController_m765E5DD565AE8A77B1BEB0988939628C3E9A1492,
+	Haptic__ctor_m2F0C7BDCD2320E80E9F7ACED8E51A5E84CEC216E,
+	Suckable_Start_m835ABDEF2DB4D13CD82DFC635317369CCE59EA68,
+	Suckable_Update_m646795073D2F6EBF69BC6814ED5D1CD70184E983,
+	Suckable_FixedUpdate_m66B57094834866670DBEC7B358D93C6A6586CB12,
+	Suckable_DeleteItem_m54C9F5B23D21A3C938DF95DB65E72ACF289F8F9A,
+	Suckable_Suck_mD553A7856DEE7C905925426F11DA0AEDBF7C9001,
+	Suckable_Shrink_mAF621E3A34371F4947D902F6980199F34A01EDAD,
+	Suckable_Flow_mE874E3E39B2F4DCA289FC02A01D35740FA7E6FC6,
+	Suckable_Swooshes_m754AF3D52BBB73DEECEA16D8CF3E973B4C4D4424,
+	Suckable__ctor_mF233F4A12E9F47A487E4540F400B4DB4A93F71DE,
+	SuckingMachineCollectionController_Start_mB8268FF23825787BBCBF66687E5C473E2ACB57B1,
+	SuckingMachineCollectionController_OnTriggerEnter_mC9F3365F2F2BB96648495B9E875B6595DB3E2478,
+	SuckingMachineCollectionController__ctor_mFDDEE9CA9FE5D647F692CB2532CC9C3C26C39997,
+	SuckingMachineController_Start_mA645913FDB01B274F50AAB88D3BC9ADBB2CB8A3A,
+	SuckingMachineController_CalculateBarCapacity_m2BF3F48E4B904547FF5986504B474433378AA68B,
+	SuckingMachineController_Update_mE389D74A1159D5405AEC716DF57038C5BABB2DF5,
+	SuckingMachineController_UpdateCapacityBar_mBEDB3979B10AB0D42A763A7E28C16AE2909BD98D,
+	SuckingMachineController_FixedUpdate_m7E59043D06577CE273A809CE71F390A3FE747AC1,
+	SuckingMachineController_Sucking_m7C858319D15D2DB8213F153780110C138C395D8C,
+	SuckingMachineController_CooldownMechanics_mCB4DF86803FF5A264CFF8EFE4FA5F406BAD3F2FE,
+	SuckingMachineController_Shoot_m2B89AE7E9056D567BDFC6B829E8311907C963761,
+	SuckingMachineController__ctor_m3F6175B2B0561D16A00A73809E074A4B87E62B05,
+	TrashGenerator_Start_mFA7B31CCD86CF6C68010EE464FCBF5B2DDA0B004,
+	TrashGenerator_SpawnItem_m01F329A16D1A376794888B96BA5ACF21BB140ECA,
+	TrashGenerator_CalculateSpread_m0C4DB34964AEDAFE7A8D6CE6A7D4584AECE3B12F,
+	TrashGenerator_FixedUpdate_m04EEDEB186EDB4D6D377B896649159886FDD3F0F,
+	TrashGenerator_Update_m32C29032189BFDD4654EA71A4A6F1A68FF300375,
+	TrashGenerator__ctor_m58BF269AEF0865D72D36AA6A76C20108273F9A98,
+	Readme__ctor_m69C325C4C171DCB0312B646A9034AA91EA8C39C6,
+	Section__ctor_m5F732533E4DFC0167D965E5F5DB332E46055399B,
+};
+static const int32_t s_InvokerIndices[73] = 
+{
+	12099,
+	6700,
+	6700,
+	6569,
+	6700,
+	5247,
+	6700,
+	6465,
+	6569,
+	6700,
+	6569,
+	6700,
+	5278,
+	6700,
+	6700,
+	6700,
+	6465,
+	6700,
+	6700,
+	6700,
+	6700,
+	6690,
+	6629,
+	6700,
+	6700,
+	6700,
+	6700,
+	6700,
+	7134,
+	6700,
+	6700,
+	6700,
+	6700,
+	6700,
+	6700,
+	5333,
+	6700,
+	6700,
+	6700,
+	12099,
+	6700,
+	2957,
+	2957,
+	6700,
+	6700,
+	6700,
+	6700,
+	6700,
+	1547,
+	6700,
+	6700,
+	2957,
+	6700,
+	6700,
+	5278,
+	6700,
+	6700,
+	6700,
+	6700,
+	6700,
+	6700,
+	6700,
+	6700,
+	6700,
+	6700,
+	6700,
+	6700,
+	4883,
+	6700,
+	6700,
+	6700,
+	6700,
+	6700,
+};
+IL2CPP_EXTERN_C const Il2CppCodeGenModule g_AssemblyU2DCSharp_CodeGenModule;
+const Il2CppCodeGenModule g_AssemblyU2DCSharp_CodeGenModule = 
+{
+	"Assembly-CSharp.dll",
+	73,
+	s_methodPointers,
+	0,
+	NULL,
+	s_InvokerIndices,
+	0,
+	NULL,
+	0,
+	NULL,
+	0,
+	NULL,
+	NULL,
+	NULL, // module initializer,
+	NULL,
+	NULL,
+	NULL,
+};
